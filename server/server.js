44591e9f6013eea3f8e5b5ccc2ce2a169db0e056
eslamicoo912 <eslamicoo3@gmail.com>
2023-03-25 01:30:05 +0200

    set up the basic packages and start running the server

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello, posts!");
+});
+
+// database connection
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listening for ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
