e7020dc5a4a111304c25b4c378465020c73ee876
eslamicoo912 <eslamicoo3@gmail.com>
2023-03-26 05:08:19 +0200

    creating the header

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 export default function App() {
   return (
     <div className="app">
       <div className="container">
         <Router>
+          <Header />
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+import { BsFillPencilFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <nav>
+        <div className="brand">
+          <h3>Posts</h3>
+          <BsFillPencilFill className="icon" />
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
